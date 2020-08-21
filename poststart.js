a18658d4144aa21f6e60c2aef118542408c236dc
ac2pic <ac2pic@gmail.com>
2020-08-21 10:56:50 -0500

    Removed deprecated modsLoaded

---
+++ poststart.js
@@ -0,0 +1,16 @@
+ig.game.addons.postUpdate.push({ 
+    onPostUpdate: function() {
+        if(ig.game.playerEntity){
+            ig.vars.set("tmp.detectMouseVec3", Vec3.createC(ig.game.playerEntity.getCombatantRoot().gui.crosshair.coll.pos.x,ig.game.playerEntity.getCombatantRoot().gui.crosshair.coll.pos.y+ig.game.playerEntity.coll.pos.z,ig.game.playerEntity.coll.pos.z));
+        }
+        if(ig.input.pressed("aim")){
+            ig.vars.set("tmp.detectMousePress", 1);
+        }
+        if(ig.input.pressed("melee")){
+            ig.vars.set("tmp.detectMeleePress", 1);
+        }
+        if(ig.input.pressed("special")){
+            ig.vars.set("tmp.detectSpecialPress", 1);
+        }
+    } 
+});
